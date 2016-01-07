bf8cc72d64a96576faa7ea301254f66b3ea4c1f1
steedos <zhuangjianguo@beiyongMac.local>
2016-01-07 11:11:42 +0800

    initial version

---
+++ app/src/main/java/org/diego/android/crosswalkdemo/MainActivity.java
@@ -0,0 +1,58 @@
+package org.diego.android.crosswalkdemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private XWalkView xWalkWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
+        xWalkWebView.load("http://hotlong.vicp.net:2180", null);
+
+        // turn on debugging
+        XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (xWalkWebView != null) {
+            xWalkWebView.pauseTimers();
+            xWalkWebView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (xWalkWebView != null) {
+            xWalkWebView.resumeTimers();
+            xWalkWebView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (xWalkWebView != null) {
+            xWalkWebView.onDestroy();
+        }
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setRequestedOrientation(Configuration.ORIENTATION_PORTRAIT);
+    }
+
+}
